cfa0d58524e29bf6a4b445c82dae704a44efcfa6
magoojoao <jhcraque@gmail.com>
2026-09-15 19:55:56 -0300

    revert commit

--- app/src/main/java/com/joaomantellato/calculadorajoaomantellato/ui/theme/Type.kt
+++ app/src/main/java/com/joaomantellato/calculadorajoaomantellato/ui/theme/Type.kt
@@ -6,18 +6,29 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+// Set of Material typography styles to start with
 val Typography = Typography(
-    // Estilo grande e limpo para o Display
-    displayLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Light,
-        fontSize = 44.sp,
-        letterSpacing = (-1).sp
-    ),
-    // Estilo dos botões
-    labelLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Medium,
-        fontSize = 18.sp
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
 )
